d0cfb037c50656ac8b63b88f393c2a57a885a419
simmygarg <40742886+simmygarg@users.noreply.github.com>
2021-07-13 23:36:59 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+# include <cs50.h>
+int main(void)
+    {
+        /* first c program after 15 years */
+        string name = get_string ("Tell your name stupid\n");
+        
+        printf("hello, %s\n",name);
+    }
